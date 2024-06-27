aaae3463a3d687043c0c88c5ebfef261a5359d62
IAntigrippinI <kovalskii.bi@dvfu.ru>
2024-06-27 20:06:42 +1000

    refactor code

--- backend/db_utils.py
+++ backend/db_utils.py
@@ -3,10 +3,12 @@ from sqlalchemy.orm import sessionmaker
 from cred import DB_URL
 from models_sql import ibm
 
+
 def create_session():
     engine = create_engine(DB_URL)
     SessionClass = sessionmaker(bind=engine)
     return SessionClass()
+
 
 def get_last_date():
     db_session = create_session()
@@ -16,7 +18,7 @@ def get_last_date():
 
 if __name__ == "__main__":
     db_session = create_session()
-    row = (db_session.query(ibm).filter_by(open=123).first())
+    row = db_session.query(ibm).filter_by(open=123).first()
     print(row)
     row.high = 222
     db_session.commit()
