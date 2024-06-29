57c418b2dde175a5638f45339b574b2fca1d2437
IAntigrippinI <kovalskii.bi@dvfu.ru>
2024-06-29 13:22:37 +1000

    update for fill 3 db

--- airflow-docker/dags/dag_fill_database.py
+++ airflow-docker/dags/dag_fill_database.py
@@ -1,6 +1,7 @@
 import sqlalchemy
 import requests
 import json
+import time
 
 import pandas as pd
 import datetime as dt
@@ -11,16 +12,21 @@ from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
 
 from db_utils import create_session
-from models_sql import ibm
+from models_sql import ibm, microsoft, apple
 from utils import make_time_range
 
 from cred_airflow import API_KEY, DB_URL
 
 
-def add_row(row):
+def add_row(row, symbol):
     try:
         db_session = create_session()
-        new_row = ibm()
+        if symbol == "IBM":
+            new_row = ibm()
+        elif symbol == "MSFT":
+            new_row = microsoft()
+        else:
+            new_row = apple()
         new_row.date = row[0]
         new_row.open = row[1]
         new_row.high = row[2]
@@ -29,6 +35,7 @@ def add_row(row):
         new_row.volume = row[5]
         db_session.add(new_row)
         db_session.commit()
+        time.sleep(0.1)
     except:
         pass
 
@@ -39,7 +46,7 @@ def get_data(symbol, date):
     respocne = requests.get(url).json()
     try:
         respocne_json = respocne["Time Series (5min)"]
-        print("All done")
+        # print("All done")
     except Exception as e:
         print("something was wrong, API ", API_KEY)
         print(respocne)
@@ -59,18 +66,29 @@ def get_data(symbol, date):
     df = pd.DataFrame(
         result, columns=["date", "open", "high", "low", "close", "volume"]
     )
-    df.to_sql(symbol, engine)
+    try:
+        for row in result:
+            add_row(row, symbol)
+        # df.to_sql(symbol, engine)
+        return True
+    except:
+        return False
 
     # for row in result:
     #     add_row(row)
 
 
 def start():
-    symbol = "IBM"
-    dates = make_time_range("2024-05", "2024-06")
-    dates = ["2024-05"]
-    for date in dates:
-        get_data(symbol, date)
+    symbols = ["IBM", "MSFT", "AAPL"]
+    dates = make_time_range("2023-06", "2024-06")
+    print(f"make data range = {dates}")
+    # dates = ["2024-05"]
+    for symbol in symbols[1:]:
+        for date in dates:
+            if not get_data(symbol, date):
+                break
+            else:
+                print(f"{symbol} for {date} was handled")
 
 
 args = {
